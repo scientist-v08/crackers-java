c02ce7505426678063a111958d5f90cfbfadb59d
Manoj V <76043917+scientist-v08@users.noreply.github.com>
2026-07-16 18:50:40 +0530

    [Bug] Removed using the default Dao authentication. (#5)

---
+++ src/main/java/com/crackers/vinayakatraders/security/CustomUserDetails.java
@@ -0,0 +1,48 @@
+package com.crackers.vinayakatraders.security;
+
+import com.crackers.vinayakatraders.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record CustomUserDetails(String email, String roleName) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roleName));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
